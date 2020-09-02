21593f2cea78c2258d9a1912f002e90fa69947ca
marcelojimenezr <49251563+marcelojimenezr@users.noreply.github.com>
2020-09-02 10:07:46 -0400

    Adding gitignore

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
+console.log(process.env.SECRET);
 const app = express();
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -11,10 +14,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
